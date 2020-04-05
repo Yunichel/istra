cf60a3aff90f6aab14a55c974f96506c1380ae36
Getty <63183785+Yunichel@users.noreply.github.com>
2020-04-05 13:21:12 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// lgdjrshg
+/*ghsfuguygfgugugjhu
+sjkrhkghegjhf*/
+
+consol.log('text');
+                                  
+
+let myVar1;
+
+
+myVar1=15;
+
+consol.log(myVar1);		
+
+
+consol.log('возраст:',myVar1);
+
+
+
+let myName='Никто';
+let myVar2=true;
+
+myVar2=false;	
+
+
+consol.log(myName);	
+
+
+
+consol.log(myVar2);						  
